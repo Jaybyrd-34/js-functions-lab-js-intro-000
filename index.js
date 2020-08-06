9344c4d7d93e33d292a5418e78e5efef4d65a5f4
Jocelyn Thurn <loaferspenny@gmail.com>
2020-08-06 17:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`;
 }
 
-function happyCustomHolidayTo(holiday, name){
-  return `Happy ${hoilday}, ${name}!`
+function happyCustomHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountDown (holiday, days){
+function holidayCountDown (holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
